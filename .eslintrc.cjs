6e2f834963405926f4fe18e03d085b2f332db3bb
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2023-07-19 11:34:27 +0200

    chore: fix eslint issues regarding prettier and react query plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,13 +5,15 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:tanstack/eslint-plugin-query',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh', 'jsx-a11y'],
+  plugins: ['react-refresh', 'jsx-a11y', '@tanstack/query'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/prefer-query-object-syntax': 'error',
   },
 }
