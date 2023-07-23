7e24b5c3c7004e0d8807016c7d33d3787726b34b
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2023-07-23 12:02:13 +0200

    chore: removed @tanstack/react-query package

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import Routes from 'app/Routes.tsx'
 
-const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </React.StrictMode>,
 )
