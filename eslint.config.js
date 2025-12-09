463b7bccfe9480230494cc5dce4452596aaa3f80
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2025-12-09 21:50:45 +0100

    chore: update the dependencies

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
+import playwright from 'eslint-plugin-playwright'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+
+export default [
+  { ignores: ['dist', 'node_modules', '**/*.config.js'] },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+        JSX: 'readonly',
+      },
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      '@tanstack/query': tanstackQuery,
+      playwright: playwright,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      ...tanstackQuery.configs.recommended.rules,
+      ...playwright.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@tanstack/query/exhaustive-deps': 'error',
+      '@tanstack/query/prefer-query-object-syntax': 'error',
+      'jsx-a11y/label-has-associated-control': [
+        2,
+        {
+          controlComponents: ['TextInput', 'Checkbox', 'Select'],
+          depth: 3,
+        },
+      ],
+    },
+  },
+]
